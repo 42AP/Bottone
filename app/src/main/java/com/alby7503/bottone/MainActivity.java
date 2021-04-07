c292cb13e1fd5e80b70f418ebc4f889ec871d6af
Alberto Vona <alberto.vona24@gmail.com>
2021-04-07 22:41:10 +0200

    .

---
+++ app/src/main/java/com/alby7503/bottone/MainActivity.java
@@ -0,0 +1,24 @@
+package com.alby7503.bottone;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private final String[] nomi = { "Pamela", "Balocchi", "Beghea", "Bonfini", "Buonuomo", "Cameriere", "Cervoni",
+            "De Micheli", "Fontana", "Gallo", "Franco", "Lattanzi", "Lopatowski", "Manili", "Mena", "Morelli",
+            "Paolozzi", "Pietrucci", "Petralia", "Puzzello", "Vona", "Zambrano", "Zouairi" };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(v -> {
+            int indice = new Random().nextInt(nomi.length);
+            String nome = nomi[indice];
+            ((TextView)findViewById(R.id.textView)).setText("Persona: " + nome);
+        });
+    }
+}
